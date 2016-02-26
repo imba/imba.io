c80ade481bab743afe3b26ef5af060d7afc6446f
somebee <sindre@identu.no>
2016-02-26 14:13:00 +0100

    Add scrimbla worker in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,24 @@ var plugins = [
 	})
 ]
 
-module.exports = {
-	module: {
-		loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]
-	},
+var mod = {
+	loaders: [{ "test": /\.imba$/, "loader": 'imba-loader'}]
+}
+
+module.exports = [{
+	module: mod,
 	resolveLoader: { root: path.join(__dirname, "node_modules") },
 	resolve: {extensions: ['', '.imba', '.js']},
 	entry: {client: "./src/client.imba", sandbox: "./src/sandbox.imba"},
 	output: { filename: "./www/[name].js" },
 	plugins: plugins
-}
+},{
+	entry: "./scrimbla/src/webworker",
+	output: {
+		filename: "./www/imbac.worker.js"
+	},
+	module: mod,
+	resolve: {extensions: ['', '.imba', '.js']},
+	plugins: plugins
+
+}]
