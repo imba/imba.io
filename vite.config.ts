3857d9c1003285ae3beb2dd57fafbc567828a450
abdellah <als.abdellah@gmail.com>
2022-09-23 11:49:20 +0300

    add code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,45 @@
-import { imba } from "vite-plugin-imba";
 import { defineConfig } from "vite";
+import { imba } from "vite-plugin-imba";
 import GithubActionsReporter from "vitest-github-actions-reporter-temp";
 import path from "path";
-
-const target = ['es2020'] // needed to support bigint
-
+import { visualizer } from "rollup-plugin-visualizer";
+const target = ["es2020"]; // needed to support bigint
 export default defineConfig({
-  plugins: [imba()],
+  plugins: [
+    visualizer({
+      emitFile: true,
+      file: "stats.html",
+	//   template: "network"
+    }),
+    imba(),
+  ],
   resolve: {
-	alias: {
-		"icons": path.resolve(__dirname, "./src/assets/icons/"),
-		"codicons": path.resolve(__dirname, "./src/assets/codicons/"),
-	},
-    extensions: [ '.imba', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    alias: {
+      "icons": path.resolve(__dirname, "./src/assets/icons/"),
+      "codicons": path.resolve(__dirname, "./src/assets/codicons/"),
+    },
+    extensions: [".imba", ".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
   optimizeDeps: {
-	esbuildOptions:{
-		target
-	}
+    esbuildOptions: {
+      target,
+    },
   },
   define: {
     "import.meta.vitest": "undefined",
   },
   build: {
-	target, 
+    target,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          compiler: ['imba/compiler'],
+		  imba: ['imba'],
+		  ref: ['./data/reference.js'],
+		  content: ['./data/content.json'],
+        },
+      },
+    },
   },
   test: {
     globals: true,
